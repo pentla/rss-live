92c31d41361874cc0fe359930b25fcd78d98cd7d
alex <apentlander@gmail.com>
2014-12-16 11:36:27 -0500

    Background page grabs the feed urls from local storage and requests a json form from google feeds

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,49 @@
-'use strict';
+var $ = require("jquery"),
+    fjs = require("functional.js");
 
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
+function getFeedUrls() {
+    var defer = $.Deferred();
+    chrome.storage.sync.get("urls", function (u) {
+        defer.resolve(u.urls);
+    });
+    return defer.promise();
+}
+
+function getLoadAPIUrl() {
+    return "https://ajax.googleapis.com/ajax/services/feed/load";
+}
+
+function getFeedLoadUrl(feedUrl) {
+    return getLoadAPIUrl() + "?v=1.0&q=" + feedUrl;
+}
+
+function getFeedJson(feedUrl) {
+    console.log(feedUrl);
+    var feedLoadUrl = getFeedLoadUrl(feedUrl);
+    var json = $.getJSON(feedLoadUrl).promise();
+    return json;
+}
+
+function setFeedItems(feedJson) {
+    var feedUrl = feedJson.responseData.feed.feedUrl;
+    var feedData = feedJson.responseData.feed;
+    var feedItem = {feedUrl: feedData};
+    console.log(feedItem);
+    chrome.storage.local.set(feedItem);
+}
+
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    console.log("Alarm fired");
+    if (alarm.name == "feedRefresh") {
+        getFeedUrls()
+            .then(fjs.map(getFeedJson))
+            .then(fjs.map(setFeedItems));
+    }
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
-
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+chrome.runtime.onInstalled.addListener(function () {
+    console.log("Installed");
+    console.log("Creating alarm");
+    var alarmData = {"delayInMinutes": 0.0, "periodInMinutes": 5.0};
+    chrome.alarms.create("feedRefresh", alarmData);
+});
