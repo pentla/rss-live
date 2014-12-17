db62d27c8bc501f48f8d55d112d50685ecacae5b
alex <apentlander@gmail.com>
2014-12-17 13:35:30 -0500

    Updated background to use the google feeds js library properly

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,49 +1,63 @@
-var $ = require("jquery"),
-    fjs = require("functional.js");
+var R = require("ramda");
+google.load("feeds", "1");
+
+// Sets a promise that resolves then the google feeds load
+var googleLoadPromise = new Promise(function (resolve, reject) {
+    google.setOnLoadCallback(function () {
+        console.log("Google loaded");
+        resolve();
+    });
+});
 
 function getFeedUrls() {
-    var defer = $.Deferred();
-    chrome.storage.sync.get("urls", function (u) {
-        defer.resolve(u.urls);
+    var promise = new Promise(function (resolve, reject) {
+        chrome.storage.sync.get("urls", function (u) {
+            resolve(u.urls);
+        });
     });
-    return defer.promise();
-}
-
-function getLoadAPIUrl() {
-    return "https://ajax.googleapis.com/ajax/services/feed/load";
-}
-
-function getFeedLoadUrl(feedUrl) {
-    return getLoadAPIUrl() + "?v=1.0&q=" + feedUrl;
+    return promise;
 }
 
 function getFeedJson(feedUrl) {
     console.log(feedUrl);
-    var feedLoadUrl = getFeedLoadUrl(feedUrl);
-    var json = $.getJSON(feedLoadUrl).promise();
-    return json;
+    var promise = new Promise(function (resolve, reject) {
+        var feed = new google.feeds.Feed(feedUrl);
+        feed.load(function (result) {
+            if (!result.error) {
+                resolve(result);
+            } else {
+                reject(result.error);
+            }
+        });
+    });
+    return promise;
 }
 
 function setFeedItems(feedJson) {
-    var feedUrl = feedJson.responseData.feed.feedUrl;
-    var feedData = feedJson.responseData.feed;
-    var feedItem = {feedUrl: feedData};
+    var feedUrl = feedJson.feed.feedUrl;
+    var feedData = feedJson.feed;
+    var feedItem = R.assoc(feedUrl, feedData, {});
     console.log(feedItem);
     chrome.storage.local.set(feedItem);
+}
+
+function refreshFeeds() {
+    getAndSet = R.pPipe(getFeedJson, setFeedItems);
+    googleLoadPromise
+        .then(getFeedUrls)
+        .then(function (urls) {
+            R.forEach(getAndSet, urls);
+        });
 }
 
 chrome.alarms.onAlarm.addListener(function (alarm) {
     console.log("Alarm fired");
     if (alarm.name == "feedRefresh") {
-        getFeedUrls()
-            .then(fjs.map(getFeedJson))
-            .then(fjs.map(setFeedItems));
+        refreshFeeds();
     }
 });
 
 chrome.runtime.onInstalled.addListener(function () {
-    console.log("Installed");
-    console.log("Creating alarm");
     var alarmData = {"delayInMinutes": 0.0, "periodInMinutes": 5.0};
     chrome.alarms.create("feedRefresh", alarmData);
 });
