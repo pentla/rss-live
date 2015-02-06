ac6cc335134fcb17da957a3a263ea63f8867f84a
Alexander Pentland <apentlander@gmail.com>
2015-02-05 21:24:14 -0800

    Options were added to the entries to allow for component reuse

--- app/scripts/components/entryitem.js
+++ app/scripts/components/entryitem.js
@@ -3,15 +3,33 @@ var React = require('react'),
 
 var EntryItem = React.createClass({
 
+    getDefaultProps: function() {
+        return {
+            feed: {
+                title: '',
+                link: ''
+            },
+            options: {
+                showContentSnippet: true,
+                headingButtons: '',
+                childElement: ''
+            }
+        };
+    },
+
     render: function() {
         log.debug('entry item props:', this.props);
         var feed = this.props.feed;
+        var opts = this.props.options;
+        var contentSnippet = <p dangerouslySetInnerHTML={{__html: feed.contentSnippet}} />
         return (
             <div className="entryItem" >
                 <h1>
                     <a dangerouslySetInnerHTML={{__html: feed.title}} href={feed.link} target='_new'/>
+                    {opts.headingButtons}
                 </h1>
-                <p dangerouslySetInnerHTML={{__html: feed.contentSnippet}} />
+                {opts.showContentSnippet ? contentSnippet : ''}
+                {opts.childElement}
             </div>
         );
     }
--- app/scripts/components/entrylist.js
+++ app/scripts/components/entrylist.js
@@ -5,16 +5,27 @@ var React = require('react'),
 
 var EntryList = React.createClass({
 
+    getDefaultProps: function() {
+        return {
+            entries: []
+        };
+    },
+
     render: function() {
         log.debug('Entries in EntryList:', this.props.entries);
+        var {entries, ...other} = this.props;
         var entryItems = R.map(function (entry) {
             log.debug(entry);
-            return (
-                <EntryItem feed={entry} />
-            );
+            try {
+                return (
+                    <EntryItem feed={entry} {...other} />
+                );
+            } catch (error) {
+                log.warn('EntryList could not make entry item:', error);
+            }
         });
 
-        var feedNodes = entryItems(this.props.entries);
+        var feedNodes = entryItems(entries);
         return (
             <div className="entryList">
                 {feedNodes}
